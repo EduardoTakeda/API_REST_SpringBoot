a00221b79182e99caefdb829be82c65df9481349
EduardoTakeda <eduardotakeda11@yahoo.com.br>
2021-06-21 16:47:22 -0300

    DTO...

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -13,8 +13,8 @@ public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     @Mapping(target="birthDate", source="birthDate", dateFormat="dd-MM-yyyy")
-    Person toModel(PersonDTO personDTO);
+    Person toModel(PersonDTO dto);
 
-    PersonDTO toDTO(Person person);
+    PersonDTO toDTO(Person dto);
 
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -2,14 +2,19 @@ package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entities.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import one.digitalinnovation.personapi.response.MessageResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private  PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE; //o INSTANCE foi criada na classe PersonMapper
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -17,16 +22,20 @@ public class PersonService {
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getLastName())
-                .birthDate(personDTO.getBirthDate())
-                .build();
+        Person personToSave = personMapper.toModel(personDTO);
 
-        Person savedPerson = personRepository.save(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO //utilizando o builder e build fora do construtor
                 .builder()
-                .message("Created person with ID "+ savedPerson.getId())
+                .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
